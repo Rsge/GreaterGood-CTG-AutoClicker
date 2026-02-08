3c8983bfb6fe46f4df07c08d2278341bf29b0aae
Jan <rsge@web.de>
2026-02-08 22:53:37 +0100

    Add automatic login

--- GreaterGood CTG AutoClicker.js
+++ GreaterGood CTG AutoClicker.js
@@ -5,7 +5,7 @@
 // @description    Automatically clicks through all the buttons on all subsites of the GreaterGood ClickToGive program every two hours.
 // @description:de Klickt sich automatisch alle zwei Std. durch alle Buttons auf allen Seiten des GreaterGood-ClickToGive-Programms.
 
-// @version        3.1.0
+// @version        3.2.0
 // @copyright      2023+, Jan G. (Rsge)
 // @license        Mozilla Public License 2.0
 // @icon           https://http-aws.greatergood.com/img/ggc/favicon-96x96.png
@@ -26,12 +26,15 @@
 (async function () {
   'use strict';
 
+  // (Try to) log in before clicking to collect points.
+  // Requires (automatic or copypasted) filling of login data.
+  const DO_LOGIN = true;
   // Max amount of seconds to wait before clicking button
   const MAX_RANDOM_TO_CLICK_SECONDS = 3;
   // Amount of seconds to wait for site to load after click
-  const WAIT_SECONDS = 2
+  const WAIT_SECONDS = 2;
   // Max amount of tries to wait for buttons to load before giving up
-  const MAX_TRIES = 5
+  const MAX_TRIES = 5;
   // Minutes between possible click-throughs
   const INTERVAL_MINUTES = 120;
   // Warning for buttons not found
@@ -45,9 +48,50 @@
     return s * 1000;
   }
 
+  // Checks for input password. Clicks if there is one, otherwise waits for input to click.
+  function checkPwdLogin(pwdInput, button, inputLength=0) {
+    if (pwdInput?.value.length > 0) {
+      //console.log(button);
+      button.click();
+    } else {
+      pwdInput.addEventListener("input", function() {
+        if(pwdInput?.value.length >= inputLength) {
+          button.click();
+        }
+      });
+    }
+  }
+
   // Vars
   let i;
   let progButtonsLeft;
+  if (DO_LOGIN) {
+    let url = new URL(window.location.href);
+    if (url.pathname == "/account/login") {
+      // Login site
+      // Log in.
+      let buttons = document.getElementsByTagName("BUTTON");
+      let loginButton = buttons[buttons.length-1];
+      //let emailInput = document.getElementById("CustomerEmail");
+      let pwdInput = document.getElementById("CustomerPassword");
+      await sleep(sToMs(WAIT_SECONDS));
+      checkPwdLogin(pwdInput, loginButton);
+      return;
+    } else if (url.pathname == "/account") {
+      // Account site
+      // Go back to CTG site.
+      document.getElementsByClassName("header__heading-logo-wrapper")[0].click();
+      return;
+    } else {
+      // Main CTG site
+      // Open login page if not already logged in.
+      let loginButtons = document.getElementsByClassName("header__icon--account link focus-inset");
+      if (loginButtons.length > 0 && loginButtons[0].href.endsWith("login")) {
+        loginButtons[0].click();
+        return;
+      }
+    }
+  }
   do {
     // When a CTG-button is present, click it.
     let ctgButton = document.getElementById("ctg-click-box-button");
